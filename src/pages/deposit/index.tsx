2b1de369b9e834725544565b2dac1e2e7c15550b
gouflv <lv.gouf@gmail.com>
2019-11-21 21:02:14 +0800

    init deposit

--- src/pages/deposit/index.tsx
+++ src/pages/deposit/index.tsx
@@ -6,59 +6,65 @@ const Page: Taro.FC = () => {
   return (
     <View className='page page--gray'>
       <View className='page-banner'>
-        <Image src={require('../../assets/pay_icon@3x.png')} mode='aspectFit' />
-        <View className='money red'>
-          <Text className='money-unit'>¥</Text>
-          199
-        </View>
-        <View className='title'>押金总额</View>
-        <View className='desc gray'>
-          押金随时退, 无需舍和退款秒到账
+        <Image src={require('../../assets/deposit_bg@3x.png')} mode='aspectFill' className='bg' />
+        <View className='content'>
+          <View className='top'>押金</View>
+          <View className='money red'>
+            <Text className='money-unit'>¥</Text>
+            199
+          </View>
+          <Button className='btn btn--round' size='mini'>补缴押金</Button>
         </View>
       </View>
 
       <View className='page-section'>
+        <View className='section-header'>
+          押金记录
+        </View>
         <View className='card'>
           <View className='cell-group'>
             <View className='cell'>
-              <View className='cell__bd'>押金总额</View>
+              <View className='cell__bd'>
+                <View className='label'>[缴纳]缴纳押金</View>
+                <Text className='desc gray'>2019/11/12 18:00</Text>
+              </View>
               <View className='cell__ft'>
-                <View className='money'>
-                  <Text className='money-unit'>¥</Text>
+                <View className='money red'>
+                  +<Text className='money-unit money-unit--large'>¥</Text>
                   99
                 </View>
               </View>
             </View>
             <View className='cell'>
-              <View className='cell__bd'>我的可用押金</View>
+              <View className='cell__bd'>
+                <View className='label'>[缴纳]缴纳押金</View>
+                <Text className='desc gray'>2019/11/12 18:00</Text>
+              </View>
               <View className='cell__ft'>
-                <View className='money'>
-                  <Text className='money-unit'>¥</Text>
+                <View className='money green'>
+                  -<Text className='money-unit money-unit--large'>¥</Text>
                   99
                 </View>
-              </View>
-            </View>
-            <View className='cell'>
-              <View className='cell__bd' />
-              <View className='cell__ft'>
-                需缴押金：
-                <Text className='money red'>
-                  <Text className='money-unit'>¥</Text>
-                  99
-                </Text>
               </View>
             </View>
           </View>
         </View>
       </View>
 
-      <View className='page-section buy-rules' style={{ paddingTop: 0 }}>
-        <View className='buy-rules-content center'>
-          确认购买即视为已同意<Text className='orange'>《借阅卡卡权益及服务规则》</Text>
+      <View className='page-section' style={{ paddingTop: 0 }}>
+        <View className='card'>
+          <View className='cell-group'>
+            <View className='cell'>
+              <View className='cell__bd orange'>
+                退回押金
+              </View>
+              <View className='cell-ft'></View>
+              <View className='cell__link'>
+                <Image src={require('../../assets/list_btn_more@3x.png')} mode='aspectFit' />
+              </View>
+            </View>
+          </View>
         </View>
-        <Button className='btn-block btn-primary btn--round'>
-          确认支付199元押金
-        </Button>
       </View>
     </View>
   )
